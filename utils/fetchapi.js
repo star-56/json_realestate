6d5db36fcd7aedeb6c8e71149984d58e99f80aba
Lokesh Agarwal <88542327+star-56@users.noreply.github.com>
2022-04-05 19:55:23 +0530

    Update fetchapi.js

--- utils/fetchapi.js
+++ utils/fetchapi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': 'e2e0fbe637msh784799c5fa93919p1e59fcjsne5e8b990cd65' ,
     },
   });
     
